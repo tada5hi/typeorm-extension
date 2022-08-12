5b58d471aa4bece91173af11ffde8f81ee9f05b9
Christian Käslin <christian.kaeslin@steiner.ch>
2022-08-12 15:43:02 +0200

    fix: adjust user seed

--- test/data/seed/user.ts
+++ test/data/seed/user.ts
@@ -6,7 +6,7 @@ export default class UserSeeder implements Seeder {
     public async run(
         dataSource: DataSource,
         factoryManager?: SeederFactoryManager
-    ) : Promise<void> {
+    ) : Promise<any> {
         const repository =  dataSource.getRepository(User);
         await repository.insert([
             {firstName: 'Caleb', lastName: 'Barrows', email: 'caleb.barrows@gmail.com'}
@@ -14,11 +14,16 @@ export default class UserSeeder implements Seeder {
 
         // ---------------------------------------------------
 
+        const users = [] as any[];
         const userFactory = await factoryManager.get(User);
         // save 1 factory generated entity, to the database
-        await userFactory.save();
+        const user1 = await userFactory.save();
+        users.push(user1);
 
         // save 5 factory generated entities, to the database
-        await userFactory.saveMany(5);
+        const users2 = await userFactory.saveMany(5);
+        users.push(users2);
+
+        return users;
     }
 }
