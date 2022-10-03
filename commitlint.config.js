9e444327ff181507900b888f6744880ee9164f2d
tada5hi <peter.placzek1996@gmail.com>
2022-10-03 18:37:45 +0200

    build: add husky, commitlint & semantic release for better development

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2022.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+module.exports = {
+    extends: ['@commitlint/config-angular'],
+};
---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/npm",
+        "@semantic-release/git"
+    ]
+}
