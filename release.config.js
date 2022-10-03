745687ab7de13e88e6c01ed2ce1e137d8cdd0a4e
tada5hi <peter.placzek1996@gmail.com>
2022-10-03 21:00:26 +0200

    build: do not attempt to sign commit + added github release plugin

--- release.config.js
+++ release.config.js
@@ -4,6 +4,7 @@ module.exports = {
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
         "@semantic-release/npm",
-        "@semantic-release/git"
+        "@semantic-release/git",
+        "@semantic-release/github"
     ]
 }
