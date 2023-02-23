87a1eee7fc3cde4ab3ee7c359ea1f8345dac4cfa
tada5hi <peter.placzek1996@gmail.com>
2023-02-23 10:08:47 +0100

    build: simplified commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2022.
- * Author Peter Placzek (tada5hi)
- * For the full copyright and license information,
- * view the LICENSE file that was distributed with this source code.
- */
-
 module.exports = {
     extends: ['@tada5hi/commitlint-config'],
 };
