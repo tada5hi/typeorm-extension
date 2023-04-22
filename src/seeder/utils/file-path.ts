7b9f46237775ccd7f10efb93c9b8b23aebb20e5a
LuisK Ruiz <12603425+inglkruiz@users.noreply.github.com>
2023-04-22 13:22:48 +0200

    feat: sort by filename when using seed pattern (#546)

--- src/seeder/utils/file-path.ts
+++ src/seeder/utils/file-path.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore next */
-import path from 'path';
+import type { LocatorInfo } from 'locter';
 import { locateMany } from 'locter';
+import path from 'path';
 
 export async function resolveFilePatterns(
     filesPattern: string[],
@@ -12,7 +13,7 @@ export async function resolveFilePatterns(
             ...(root ? { path: root } : {}),
             ignore: ['**/*.d.ts'],
         },
-    ).then((files) => files.map((el) => path.join(el.path, el.name + el.extension)));
+    ).then(buildFilePathname);
 }
 
 export function resolveFilePaths(
@@ -24,4 +25,14 @@ export function resolveFilePaths(
             filePath :
             path.resolve(root || process.cwd(), filePath)
     ));
+}
+
+/**
+ * Exported only for testing purposes
+ */
+export function buildFilePathname(files: LocatorInfo[]) {
+    return (
+        // sorting by name so that we can define the order of execution using file names
+        files.sort((a, b) => (a.name > b.name ? 1 : -1)).map((el) => path.join(el.path, el.name + el.extension))
+    );
 }
