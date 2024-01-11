b4f26cd65e6237c924b482861309aafaf5846331
tada5hi <peter.placzek1996@gmail.com>
2024-01-11 12:16:31 +0100

    test: add test suite for merging data source options

---
+++ test/unit/data-source/options/merge.spec.ts
@@ -0,0 +1,32 @@
+import type { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { mergeDataSourceOptions } from '../../../../src';
+
+describe('src/data-source/options/merge', () => {
+    it('should merge data source options', () => {
+        const options = mergeDataSourceOptions({
+            type: 'postgres',
+            password: undefined,
+        }, {
+            type: 'postgres',
+            password: 'password',
+        });
+
+        expect(options).toBeDefined();
+        expect(options.type).toEqual('postgres');
+        expect((options as PostgresConnectionOptions).password).toEqual('password');
+    });
+
+    it('should not merge data source options', () => {
+        const options = mergeDataSourceOptions({
+            type: 'postgres',
+            password: undefined,
+        }, {
+            type: 'mysql',
+            password: 'password',
+        });
+
+        expect(options).toBeDefined();
+        expect(options.type).toEqual('postgres');
+        expect((options as PostgresConnectionOptions).password).toBeUndefined();
+    });
+});
